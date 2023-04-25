82dedeba079b0169aab32a03ce5b6cf38589094d
ramen6315 <ramen6315@gmail.com>
2023-04-25 09:51:28 +0900

    import optimize

--- test/java/com/example/demo/TestServiceTest.java
+++ test/java/com/example/demo/TestServiceTest.java
@@ -1,14 +1,10 @@
 package com.example.demo;
 
-import com.example.demo.search.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @SpringBootTest
